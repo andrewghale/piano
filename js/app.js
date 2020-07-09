f37145c93cc784381e89c23f09ceee572a8046ee
Andrew Hale <andyhalebmx@hotmail.com>
2020-07-09 19:41:38 +0100

    refactoring

--- js/app.js
+++ js/app.js
@@ -3,12 +3,11 @@
 // const note3 = document.getElementById("note3Field")
 // const note4 = document.getElementById("note4Field")
 // const button = document.getElementById("submitGuess")
-// const result = document.getElementById("result")
+const result = document.getElementById("result")
 
-let allowed = true;
+let allowed = true
 let notesArr = []
 let skippedNote = false
-
 const chordTypes = [
   {
     "code": ".",
@@ -23,13 +22,33 @@ const chordTypes = [
     "name": "m",
   },
   {
+    "code": ".6",
+    "name": "♭5",
+  },
+  {
     "code": ".7",
     "name": "5",
   },
   {
     "code": ".4.7",
-    "name": "",
+    "name": ""
   },
+  {
+    "code": ".3.8",
+    "name": "first inv"
+  },
+  {
+    "code": ".5.9",
+    "name": " second inv"
+  },
+  // {
+  //   "code": {
+  //     ".4.7": "root",
+  //     ".3.8": "first inversion",
+  //     ".5.9": "second inverstion"
+  //   },
+  //   "name": "",
+  // },
   {
     "code": ".5.7",
     "name": "sus4"
@@ -67,6 +86,10 @@ const chordTypes = [
     "name": "maj7"
   },
   {
+    "code": ".4.7.11.2",
+    "name": "maj7/9"
+  },
+  {
     "code": ".5.7.11",
     "name": "sus4/maj7"
   },
@@ -96,7 +119,7 @@ const chordTypes = [
   },
   {
     "code": ".3.7.8",
-    "name": "mflat6"
+    "name": "m ♭6"
   },
   {
     "code": ".3.7.10",
@@ -108,7 +131,7 @@ const chordTypes = [
   },
   {
     "code": ".3.6.10",
-    "name": "m7flat5"
+    "name": "m7 ♭5"
   },
   {
     "code": ".4.7.10",
@@ -116,11 +139,11 @@ const chordTypes = [
   },
   {
     "code": ".2.7.9",
-    "name": "6sus2"
+    "name": "6 sus2"
   },
   {
     "code": ".5.7.10",
-    "name": "7sus4"
+    "name": "7 sus4"
   },
   {
     "code": ".4.8",
@@ -145,13 +168,20 @@ const chordTypes = [
   {
     "code": ".3.6.17",
     "name": "dim 11"
-  }
+  },
+  // {
+  //   "code": ".3.8",
+  //   "name": "major first inversion"
+  // },
+  // {
+  //   "code": ".5.9",
+  //   "name": "major second inversion"
+  // }
 ]
 
-const makeChordCode = (note1, note2, note3, note4) => {
-  let note1Index, note2Index, note3Index, note4Index
+const makeChordCode = (...arr) => {
   let chord = []
-  const notesArr = [
+  const notesArrChordCode = [
     "C",
     "C#",
     "D",
@@ -189,54 +219,34 @@ const makeChordCode = (note1, note2, note3, note4) => {
     "A#",
     "B"
   ]
-  note1Index = notesArr.indexOf(note1)
-  note2Index = notesArr.indexOf(note2, note1Index)
-  note3Index = notesArr.indexOf(note3, note2Index)
-  note4Index = notesArr.indexOf(note4, note3Index)
-
-  // console.log(`${note2Index - note1Index}`)
-
-  if (Math.sign(note2Index - note1Index) !== -1) {
-    chord.push(note2Index - note1Index)
-  }
-  if (Math.sign(note3Index - note1Index) !== -1) {
-    chord.push(note3Index - note1Index)
-  }
-  if (Math.sign(note4Index - note1Index) !== -1) {
-  chord.push(note4Index
-     - note1Index
-  )}
-  // console.log(`chord is ${chord}`)
+  for (let i=1; i < arr.length; i++) {
+    let current = notesArrChordCode.indexOf( arr[i] )
+    let first = notesArrChordCode.indexOf( arr[0] )
+    if (Math.sign(current - first) !== -1) {
+      chord.push(current - first)
+    } else {
+      chord.push((current - first)+12)}
+    }
   return chord
 }
 
-const findChordTypes = (str) => {
-  console.log(str)
+const findChordTypes = str => {
   const theResult = chordTypes.find((el) => el.code === str)
-  if (theResult == undefined) {
-    return
-  } else {
-    return theResult.name
-  }
+  if (theResult !== undefined)
+  return theResult.name
 }
 
-window.addEventListener('keydown', function(e) {
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"] `)
-  if ( key==null ) {
-    return
-  }
-  key.classList.add("playing")
-})
+const removePlaying = e => {
+  const keyRemove = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if ( keyRemove !== null ) keyRemove.classList.remove("playing")
+}
 
-window.addEventListener('keyup', function(e){
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-  if ( key==null ) {
-    return
-  }
-  key.classList.remove("playing")
-})
+const addPlaying = e => {
+  const keyAdd = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if ( keyAdd !== null ) keyAdd.classList.add("playing")
+}
 
-const sort = (str) => {
+// const sort = (str) => {
 //     let itemsOrdered = []
 //     const theOrder = [65, 87, 83, 69, 68, 70, 84, 71, 89, 72, 85, 74, 75, 79, 76, 80, 186, 222, 221, 220]
 //     for (var j = 0; j <= str.length; j++) {
@@ -246,49 +256,84 @@ const sort = (str) => {
 //     console.log(itemsOrdered);
 //     console.log(str)
 //     }
+// }
+
+const addNote = e => {
+  if (document.querySelector(`.key[data-key="${e.keyCode}"]`)) {
+    let oneNote = document.querySelector(`.key[data-key="${e.keyCode}"] .js-note`).textContent
+    if ( event.repeat != undefined ) {
+      allowed = !event.repeat
+    }
+    if ( !allowed ) return
+    allowed = false
+    if (notesArr.includes(oneNote))
+      skippedNote = true
+    if (oneNote && !notesArr.includes(oneNote)) {
+      notesArr.push(oneNote)
+      // const jsonObj = {
+      //   note: `${oneNote}`,
+      //   keyCode: `${e.keyCode}`
+      // }
+      // notesArr = [...notesArr, jsonObj]
+      // notesArr.push(jsonObj)
+      // testArr.push(jsonObj)
+      // notesArr = testArr
+    }
+    // console.log(testArr)
+    // console.log(notesArr)
+    let theChordCode = (makeChordCode(...notesArr))
+    .join(".").toString()
+    if (theChordCode !== "") {
+      theChordCode = `.`.concat(theChordCode)
+      result.innerHTML = `Chord: ${notesArr[0]} ${findChordTypes(theChordCode)}`
+    }
+  }
 }
 
-window.addEventListener("keydown", function(e) {
-  let oneNote = document.querySelector(`.key[data-key="${e.keyCode}"] .js-note`).textContent
-  console.log(oneNote)
-  if ( event.repeat != undefined ) {
-    allowed = !event.repeat;
+const removeNote = e => {
+  if (document.querySelector(`.key[data-key="${e.keyCode}"]`)) {
+    let oneNote = document.querySelector(`.key[data-key="${e.keyCode}"] .js-note`).textContent
+
+    allowed = true
+
+    if (oneNote && skippedNote !== true) {
+      notesArr.pop(oneNote)
+    }
+
+    skippedNote = false
+
+    let theChordCode = (makeChordCode(...notesArr)).join(".").toString()
+    theChordCode = `.`.concat(theChordCode)
+
+    if (notesArr === undefined || notesArr.length === 0) {
+      result.innerHTML = `Chord:`
+    } else {
+      result.innerHTML = `Chord: ${notesArr[0]} ${findChordTypes(theChordCode)}`
+    }
   }
-  if ( !allowed ) return
-  allowed = false;
-  if (notesArr.includes(oneNote)) {
-    skippedNote = true
-  }
-  if (oneNote && !notesArr.includes(oneNote)) {
-    notesArr.push(oneNote)
-  }
-  let theChordCode = (makeChordCode(...notesArr)).join(".").toString()
-  if (theChordCode !== "") {
-    let firstDot = `.`
-    theChordCode = firstDot.concat(theChordCode)
-    result.innerHTML = `Chord: ${notesArr[0]} ${findChordTypes(theChordCode)}`
-  }
+}
+
+window.addEventListener("keydown", e => {
+  addNote(e)
+  addPlaying(e)
 })
 
-window.addEventListener("keyup", function(e){
-  let oneNote = document.querySelector(`.key[data-key="${e.keyCode}"] .js-note`).textContent
-  allowed = true;
-  if (oneNote && skippedNote !== true) {
-    notesArr.pop(oneNote)
-  }
-  skippedNote = false
-  let theChordCode = (makeChordCode(...notesArr)).join(".").toString()
-  let firstDot = `.`
-  theChordCode = firstDot.concat(theChordCode)
-  if (notesArr === undefined || notesArr.length === 0) {
-    console.log(`empty array`)
-    result.innerHTML = `Chord:`
-  } else {
-  result.innerHTML = `Chord: ${notesArr[0]} ${findChordTypes(theChordCode)}`
-  }
+window.addEventListener("keyup", e => {
+  removeNote(e)
+  removePlaying(e)
 })
 
-
+// const sort = (str) => {
+//     let itemsOrdered = []
+//     const theOrder = [65, 87, 83, 69, 68, 70, 84, 71, 89, 72, 85, 74, 75, 79, 76, 80, 186, 222, 221, 220]
+//     for (var j = 0; j <= str.length; j++) {
+//         if (notesArr.indexOf(theOrder[j]) > -1) {
+//             itemsOrdered.push(theOrder[j]);
+//         }
+//     console.log(itemsOrdered);
+//     console.log(str)
+//     }
+// }
 let context = null;
 let oscillator = null;
 
