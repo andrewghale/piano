6a673d7c4a9907d579df04b75821ced480e96a1d
Andrew Hale <andyhalebmx@hotmail.com>
2020-07-13 12:05:30 +0100

    created new object of chord names

--- js/app.js
+++ js/app.js
@@ -9,261 +9,185 @@ let allowed = true
 let notesArr = []
 let skippedNote = false
 const chordTypes = [
+  { name: 'augmented', code: '0.4.8', quality: 'augmented' },
   {
-    "code": ".",
-    "name": "",
+    name: 'augmented-eleventh',
+    code: '0.4.7.10.14.18',
+    quality: 'major'
   },
   {
-    "code": ".4",
-    "name": "",
+    name: 'augmented-major-seventh',
+    code: '0.4.8.11',
+    quality: 'augmented'
+  },
+  { name: 'augmented-seventh', code: '0.4.8.10', quality: 'augmented' },
+  {
+    name: 'augmented-sixth',
+    code: '0.6.10',
+    quality: 'predominant'
+  },
+  { name: 'diminished', code: '0.3.6', quality: 'diminished' },
+  {
+    name: 'diminished-major-seventh',
+    code: '0.3.6.11',
+    quality: 'diminished'
   },
   {
-    "code": ".3",
-    "name": "m",
+    name: 'diminished-seventh',
+    code: '0.3.6.9',
+    quality: 'diminished'
+  },
+  { name: 'dominant', code: '0.4.7', quality: 'major' },
+  {
+    name: 'dominant-eleventh',
+    code: '0.4.7.10.14.17',
+    quality: 'major'
   },
   {
-    "code": ".6",
-    "name": "♭5",
+    name: 'dominant-minor-ninth',
+    code: '0.4.7.10.13',
+    quality: 'major'
+  },
+  { name: 'dominant-ninth', code: '0.4.7.10.14', quality: 'major' },
+  { name: 'dominant-parallel', code: '0.3.7', quality: 'minor' },
+  { name: 'dominant-seventh', code: '0.4.7.10', quality: 'major' },
+  {
+    name: 'dominant-seventh-flat-five',
+    code: '0.4.6.10',
+    quality: 'diminished'
   },
   {
-    "code": ".7",
-    "name": "5",
+    name: 'dominant-seventh-sharp-nine',
+    code: '0.4.7.10.15',
+    quality: 'major'
   },
   {
-    "code": ".4.7",
-    "name": ""
+    name: 'dominant-thirteenth',
+    code: '0.4.7.10.14.17.9',
+    quality: 'major'
+  },
+  { name: 'dream', code: '0.5.6.7', quality: 'just' },
+  { name: 'elektra', code: '0.7.9.13.16', quality: 'bitonal' },
+  { name: 'farben', code: '0.8.11.16.21', quality: 'atonal' },
+  {
+    name: 'half-diminished-seventh',
+    code: '0.3.6.10',
+    quality: 'diminished'
+  },
+  { name: 'harmonic-seventh', code: '0.4.7.10', quality: 'major' },
+  { name: 'leading-tone-triad', code: '0.3.6', quality: 'diminished' },
+  { name: 'lydian', code: '0.4.7.11.18', quality: 'major' },
+  { name: 'magic', code: '0.1.5.6.10.12.15.17', quality: 'just' },
+  { name: 'major', code: '0.4.7', quality: 'major' },
+  { name: 'major-eleventh', code: '0.4.7.11.14.17', quality: 'major' },
+  { name: 'major-seventh', code: '0.4.7.11', quality: 'major' },
+  {
+    name: 'major-seventh-sharp-eleventh',
+    code: '0.4.8.11.18',
+    quality: 'augmented'
+  },
+  { name: 'major-sixth', code: '0.4.7.9', quality: 'major' },
+  { name: 'major-sixth-ninth', code: '0.4.7.9.14', quality: 'major' },
+  { name: 'major-ninth', code: '0.4.7.11.14', quality: 'major' },
+  {
+    name: 'major-thirteenth',
+    code: '0.4.7.11.14.18.9',
+    quality: 'major'
+  },
+  { name: 'mediant', code: '0.3.7', quality: 'minor' },
+  { name: 'minor', code: '0.3.7', quality: 'minor' },
+  { name: 'minor-eleventh', code: '0.3.7.10.14.17', quality: 'minor' },
+  { name: 'minor-major-seventh', code: '0.3.7.11', quality: 'minor' },
+  { name: 'minor-ninth', code: '0.3.7.10.14', quality: 'minor' },
+  { name: 'minor-seventh', code: '0.3.7.10', quality: 'minor' },
+  { name: 'minor-sixth', code: '0.3.7.9', quality: 'minor' },
+  { name: 'minor-sixth-ninth', code: '0.3.7.9.14', quality: 'minor' },
+  {
+    name: 'minor-thirteenth',
+    code: '0.3.7.10.14.17.9',
+    quality: 'minor'
+  },
+  { name: 'mu', code: '0.2.4.7', quality: 'major' },
+  { name: 'mystic', code: '0.6.10.16.21.14', quality: 'atonal' },
+  { name: 'neapolitan', code: '1.5.8', quality: 'major' },
+  {
+    name: 'ninth-augmented-fifth',
+    code: '0.4.8.10.14',
+    quality: 'augmented'
+  },
+  { name: 'ninth-flat-fifth', code: '0.4.6.10.14', quality: 'm3+d5' },
+  {
+    name: 'northern-lights',
+    code: '1.2.8.12.15.18.19.22.23.28.31',
+    quality: 'atonal'
   },
   {
-    "code": ".3.8",
-    "name": "first inv"
+    name: '"ode-to-napoleon"-hexa',
+    code: '0.1.4.5.8.9',
+    quality: 'atonal'
   },
+  { name: 'petrushka', code: '0.1.4.6.7.10', quality: 'bitonal' },
+  { name: 'power-p5', code: '0.7', quality: 'indeterminate' },
+  { name: 'psalms', code: '0.3.7', quality: 'minor' },
+  { name: 'secondary-dominant', code: '0.4.7', quality: 'major' },
   {
-    "code": ".5.9",
-    "name": " second inv"
+    name: 'secondary-leading-tone',
+    code: '0.3.6',
+    quality: 'diminished'
   },
+  { name: 'secondary-supertonic', code: '0.3.7', quality: 'minor' },
+  { name: 'seven-six', code: '0.4.7.9.10', quality: 'major' },
+  {
+    name: 'seventh-suspension-four',
+    code: '0.5.7.10',
+    quality: 'suspended'
+  },
+  { name: 'so-what', code: '0.5.10.15.19', quality: 'bitonal' },
+  { name: 'suspended', code: '0.5.7', quality: 'suspended' },
+  { name: 'subdominant', code: '0.4.7', quality: 'major' },
+  { name: 'subdominant-parallel', code: '0.3.7', quality: 'minor' },
+  { name: 'submediant', code: '0.3.7', quality: 'minor' },
+  { name: 'subtonic', code: '0.4.7', quality: 'major' },
+  { name: 'supertonic', code: '0.3.7', quality: 'minor' },
   // {
-  //   "code": {
-  //     ".4.7": "root",
-  //     ".3.8": "first inversion",
-  //     ".5.9": "second inverstion"
-  //   },
-  //   "name": "",
-  // },
-  {
-    "code": ".5.7",
-    "name": "sus4"
-  },
-  {
-    "code": ".2.7",
-    "name": "sus2"
-  },
-  {
-    "code": ".1.7",
-    "name": "no3/5(♭9)"
-  },
-  {
-    "code": ".6.7",
-    "name": "no3/5(#11)"
-  },
-  {
-    "code": ".6.7.11",
-    "name": "no3/5/maj7(#11)"
-  },
-  {
-    "code": ".7.10",
-    "name": "no3/5 7"
-  },
-  {
-    "code": ".4.7.14",
-    "name": "(add9)"
-  },
-  {
-    "code": ".4.7.9",
-    "name": "6"
-  },
-  {
-    "code": ".4.7.11",
-    "name": "maj7"
-  },
-  {
-    "code": ".4.7.11.2",
-    "name": "maj7/9"
-  },
-  {
-    "code": ".5.7.11",
-    "name": "sus4/maj7"
-  },
-  {
-    "code": ".7.11",
-    "name": "no3/5/maj7"
-  },
-  {
-    "code": ".3.7",
-    "name": "m"
-  },
-  {
-    "code": ".3.7.13",
-    "name": "m add ♭9"
-  },
-  {
-    "code": ".3.7.14",
-    "name": "m add9"
-  },
-  {
-    "code": ".3.7.17",
-    "name": "m add11"
-  },
-  {
-    "code": ".3.7.9",
-    "name": "m6"
-  },
-  {
-    "code": ".3.7.8",
-    "name": "m ♭6"
-  },
-  {
-    "code": ".3.7.10",
-    "name": "m7"
-  },
-  {
-    "code": ".3.7.11",
-    "name": "m maj7"
-  },
-  {
-    "code": ".3.6.10",
-    "name": "m7 ♭5"
-  },
-  {
-    "code": ".4.7.10",
-    "name": "7"
-  },
-  {
-    "code": ".2.7.9",
-    "name": "6 sus2"
-  },
-  {
-    "code": ".5.7.10",
-    "name": "7 sus4"
-  },
-  {
-    "code": ".4.8",
-    "name": "aug"
-  },
-  {
-    "code": ".3.6",
-    "name": "dim"
-  },
-  {
-    "code": ".3.6.9",
-    "name": "dim7"
-  },
-  {
-    "code": ".3.6.13",
-    "name": "dim ♭9"
-  },
-  {
-    "code": ".3.6.14",
-    "name": "dim 9"
-  },
-  {
-    "code": ".3.6.17",
-    "name": "dim 11"
-  },
-  // {
-  //   "code": ".3.8",
-  //   "name": "major first inversion"
+  //   name: 'thirteenth-flat-ninth',
+  //   code: '0.4.7.10.13.NaN.NaN',
+  //   quality: 'major'
   // },
   // {
-  //   "code": ".5.9",
-  //   "name": "major second inversion"
-  // }
+  //   name: 'thirteenth-flat-ninth-flat-fifth',
+  //   code: '0.4.6.10.13.NaN.NaN',
+  //   quality: 'm3+d5'
+  // },
+  { name: 'tonic-counter-parallel', code: '0.3.7', quality: 'minor' },
+  { name: 'tonic', code: '0.4.7', quality: 'major' },
+  { name: 'tonic-parallel', code: '0.3.7', quality: 'minor' },
+  { name: 'tristan', code: '0.3.6.10', quality: 'predominant' },
+  { name: 'viennese-tri', code: '0.6.7', quality: 'atonal' }
 ]
 
+const sortingArr = [ "C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5","C6","C#6","D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6" ]
+
 const makeChordCode = (...arr) => {
+  // takes array of musical notes,
+  // e.g. ["C4", "D#4", "G4"]
+  // returns semitone intervals (root note included in output)
+  //e.g. [4, 7]
   let chord = []
-  // array of musical notes
-  const notesArrChordCode = [
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-    "A",
-    "A#",
-    "B",
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-    "A",
-    "A#",
-    "B",
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-    "A",
-    "A#",
-    "B"
-  ]
-  for (let i=1; i < arr.length; i++) {
-    let current = notesArrChordCode.indexOf( arr[i] )
-    let first = notesArrChordCode.indexOf( arr[0] )
-    if (Math.sign(current - first) !== -1) {
-      chord.push(current - first)
-    } else {
-      chord.push((current - first)+12)}
-    }
-  return chord
+  arr.map(c => chord.push(sortingArr.indexOf(c)))
+  return chord.map(c=>c-chord[0])
 }
 
 const findChordTypes = str => {
   // str is the chord code e.g. ".4.7.11"
-  // theResult is an object e.g. {code: ".5.7", name: "sus4"}
-  const theResult = chordTypes.find((el) => el.code === str)
-  if (theResult !== undefined)
-    return theResult.name
+  // chord is an object e.g. {code: ".5.7", name: "sus4"}
+  const chord = chordTypes.find((el) => el.code === str)
+  return chord === undefined ? `` :chord.name
 }
 
 //
 // Add and Remove CSS Classes
 // get the DOM element via data-key and use it if not null
-
-const removePlaying = e => {
-  const keyRemove = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-  if ( keyRemove !== null ) keyRemove.classList.remove(`playing`)
-}
-
-const addPlaying = e => {
-  const keyAdd = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-  if ( keyAdd !== null ) keyAdd.classList.add(`playing`)
-}
-
-// const sort = (str) => {
-//     let itemsOrdered = []
-//     const theOrder = [65, 87, 83, 69, 68, 70, 84, 71, 89, 72, 85, 74, 75, 79, 76, 80, 186, 222, 221, 220]
-//     for (var j = 0; j <= str.length; j++) {
-//         if (notesArr.indexOf(theOrder[j]) > -1) {
-//             itemsOrdered.push(theOrder[j]);
-//         }
-//     console.log(itemsOrdered);
-//     console.log(str)
-//     }
-// }
 
 const addNote = e => {
   if (document.querySelector(`.key[data-key="${e.keyCode}"]`)) {
@@ -279,11 +203,13 @@ const addNote = e => {
     if (oneNote && !notesArr.includes(oneNote)) {
       notesArr.push(oneNote)
     }
-    let theChordCode = (makeChordCode(...notesArr))
-    .join(".").toString()
+    let theChordCode = makeChordCode(...notesArr
+      .sort((a,b)=>sortingArr.indexOf(a)-sortingArr.indexOf(b)))
+      .join(".")
+      .toString()
+      console.log(theChordCode)
     if (theChordCode !== ``) {
-      theChordCode = `.`.concat(theChordCode)
-      result.innerHTML = `Chord: ${notesArr[0]} ${findChordTypes(theChordCode)}`
+      result.innerHTML = `Chord: ${notesArr[0].replace(/\d/g, '')} ${findChordTypes(theChordCode)}`
     }
   }
 }
@@ -291,24 +217,32 @@ const addNote = e => {
 const removeNote = e => {
   if (document.querySelector(`.key[data-key="${e.keyCode}"]`)) {
     let oneNote = document.querySelector(`.key[data-key="${e.keyCode}"] .js-note`).textContent
-
     allowed = true
-
     if (oneNote && skippedNote !== true) {
       notesArr.pop(oneNote)
     }
-
     skippedNote = false
-
-    let theChordCode = (makeChordCode(...notesArr)).join(`.`).toString()
-    theChordCode = `.`.concat(theChordCode)
-
+    let theChordCode = makeChordCode(...notesArr
+      .sort((a,b)=>sortingArr.indexOf(a)-sortingArr.indexOf(b)))
+      .join(".")
+      .toString()
+    console.log(theChordCode)
     if (notesArr === undefined || notesArr.length === 0) {
       result.innerHTML = `Chord:`
     } else {
-      result.innerHTML = `Chord: ${notesArr[0]} ${findChordTypes(theChordCode)}`
+      result.innerHTML = `Chord: ${notesArr[0].replace(/\d/g, '')} ${findChordTypes(theChordCode)}`
     }
   }
+}
+
+const removePlaying = e => {
+  const keyRemove = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if ( keyRemove !== null ) keyRemove.classList.remove(`playing`)
+}
+
+const addPlaying = e => {
+  const keyAdd = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if ( keyAdd !== null ) keyAdd.classList.add(`playing`)
 }
 
 window.addEventListener("keydown", e => {
@@ -321,17 +255,7 @@ window.addEventListener("keyup", e => {
   removePlaying(e)
 })
 
-// const sort = (str) => {
-//     let itemsOrdered = []
-//     const theOrder = [65, 87, 83, 69, 68, 70, 84, 71, 89, 72, 85, 74, 75, 79, 76, 80, 186, 222, 221, 220]
-//     for (var j = 0; j <= str.length; j++) {
-//         if (notesArr.indexOf(theOrder[j]) > -1) {
-//             itemsOrdered.push(theOrder[j]);
-//         }
-//     console.log(itemsOrdered);
-//     console.log(str)
-//     }
-// }
+
 let context = null;
 let oscillator = null;
 
--- js/intervals.js
+++ js/intervals.js
@@ -9,192 +9,180 @@ let allowed = true
 let notesArr = []
 let skippedNote = false
 const chordTypes = [
+  { name: 'augmented', code: '0.4.8', quality: 'augmented' },
   {
-    "code": ".",
-    "name": "",
+    name: 'augmented-eleventh',
+    code: '0.4.7.10.14.18',
+    quality: 'major'
   },
   {
-    "code": ".4",
-    "name": "",
+    name: 'augmented-major-seventh',
+    code: '0.4.8.11',
+    quality: 'augmented'
+  },
+  { name: 'augmented-seventh', code: '0.4.8.10', quality: 'augmented' },
+  {
+    name: 'augmented-sixth',
+    code: '0.6.10',
+    quality: 'predominant'
+  },
+  { name: 'diminished', code: '0.3.6', quality: 'diminished' },
+  {
+    name: 'diminished-major-seventh',
+    code: '0.3.6.11',
+    quality: 'diminished'
   },
   {
-    "code": ".3",
-    "name": "m",
+    name: 'diminished-seventh',
+    code: '0.3.6.9',
+    quality: 'diminished'
+  },
+  { name: 'dominant', code: '0.4.7', quality: 'major' },
+  {
+    name: 'dominant-eleventh',
+    code: '0.4.7.10.14.17',
+    quality: 'major'
   },
   {
-    "code": ".6",
-    "name": "♭5",
+    name: 'dominant-minor-ninth',
+    code: '0.4.7.10.13',
+    quality: 'major'
+  },
+  { name: 'dominant-ninth', code: '0.4.7.10.14', quality: 'major' },
+  { name: 'dominant-parallel', code: '0.3.7', quality: 'minor' },
+  { name: 'dominant-seventh', code: '0.4.7.10', quality: 'major' },
+  {
+    name: 'dominant-seventh-flat-five',
+    code: '0.4.6.10',
+    quality: 'diminished'
   },
   {
-    "code": ".7",
-    "name": "5",
+    name: 'dominant-seventh-sharp-nine',
+    code: '0.4.7.10.15',
+    quality: 'major'
   },
   {
-    "code": ".4.7",
-    "name": ""
+    name: 'dominant-thirteenth',
+    code: '0.4.7.10.14.17.9',
+    quality: 'major'
+  },
+  { name: 'dream', code: '0.5.6.7', quality: 'just' },
+  { name: 'elektra', code: '0.7.9.13.16', quality: 'bitonal' },
+  { name: 'farben', code: '0.8.11.16.21', quality: 'atonal' },
+  {
+    name: 'half-diminished-seventh',
+    code: '0.3.6.10',
+    quality: 'diminished'
+  },
+  { name: 'harmonic-seventh', code: '0.4.7.10', quality: 'major' },
+  { name: 'leading-tone-triad', code: '0.3.6', quality: 'diminished' },
+  { name: 'lydian', code: '0.4.7.11.18', quality: 'major' },
+  { name: 'magic', code: '0.1.5.6.10.12.15.17', quality: 'just' },
+  { name: 'major', code: '0.4.7', quality: 'major' },
+  { name: 'major-eleventh', code: '0.4.7.11.14.17', quality: 'major' },
+  { name: 'major-seventh', code: '0.4.7.11', quality: 'major' },
+  {
+    name: 'major-seventh-sharp-eleventh',
+    code: '0.4.8.11.18',
+    quality: 'augmented'
+  },
+  { name: 'major-sixth', code: '0.4.7.9', quality: 'major' },
+  { name: 'major-sixth-ninth', code: '0.4.7.9.14', quality: 'major' },
+  { name: 'major-ninth', code: '0.4.7.11.14', quality: 'major' },
+  {
+    name: 'major-thirteenth',
+    code: '0.4.7.11.14.18.9',
+    quality: 'major'
+  },
+  { name: 'mediant', code: '0.3.7', quality: 'minor' },
+  { name: 'minor', code: '0.3.7', quality: 'minor' },
+  { name: 'minor-eleventh', code: '0.3.7.10.14.17', quality: 'minor' },
+  { name: 'minor-major-seventh', code: '0.3.7.11', quality: 'minor' },
+  { name: 'minor-ninth', code: '0.3.7.10.14', quality: 'minor' },
+  { name: 'minor-seventh', code: '0.3.7.10', quality: 'minor' },
+  { name: 'minor-sixth', code: '0.3.7.9', quality: 'minor' },
+  { name: 'minor-sixth-ninth', code: '0.3.7.9.14', quality: 'minor' },
+  {
+    name: 'minor-thirteenth',
+    code: '0.3.7.10.14.17.9',
+    quality: 'minor'
+  },
+  { name: 'mu', code: '0.2.4.7', quality: 'major' },
+  { name: 'mystic', code: '0.6.10.16.21.14', quality: 'atonal' },
+  { name: 'neapolitan', code: '1.5.8', quality: 'major' },
+  {
+    name: 'ninth-augmented-fifth',
+    code: '0.4.8.10.14',
+    quality: 'augmented'
+  },
+  { name: 'ninth-flat-fifth', code: '0.4.6.10.14', quality: 'm3+d5' },
+  {
+    name: 'northern-lights',
+    code: '1.2.8.12.15.18.19.22.23.28.31',
+    quality: 'atonal'
   },
   {
-    "code": ".3.8",
-    "name": "first inv"
+    name: '"ode-to-napoleon"-hexa',
+    code: '0.1.4.5.8.9',
+    quality: 'atonal'
   },
+  { name: 'petrushka', code: '0.1.4.6.7.10', quality: 'bitonal' },
+  { name: 'power-p5', code: '0.7', quality: 'indeterminate' },
+  { name: 'psalms', code: '0.3.7', quality: 'minor' },
+  { name: 'secondary-dominant', code: '0.4.7', quality: 'major' },
   {
-    "code": ".5.9",
-    "name": " second inv"
+    name: 'secondary-leading-tone',
+    code: '0.3.6',
+    quality: 'diminished'
   },
+  { name: 'secondary-supertonic', code: '0.3.7', quality: 'minor' },
+  { name: 'seven-six', code: '0.4.7.9.10', quality: 'major' },
+  {
+    name: 'seventh-suspension-four',
+    code: '0.5.7.10',
+    quality: 'suspended'
+  },
+  { name: 'so-what', code: '0.5.10.15.19', quality: 'bitonal' },
+  { name: 'suspended', code: '0.5.7', quality: 'suspended' },
+  { name: 'subdominant', code: '0.4.7', quality: 'major' },
+  { name: 'subdominant-parallel', code: '0.3.7', quality: 'minor' },
+  { name: 'submediant', code: '0.3.7', quality: 'minor' },
+  { name: 'subtonic', code: '0.4.7', quality: 'major' },
+  { name: 'supertonic', code: '0.3.7', quality: 'minor' },
   // {
-  //   "code": {
-  //     ".4.7": "root",
-  //     ".3.8": "first inversion",
-  //     ".5.9": "second inverstion"
-  //   },
-  //   "name": "",
-  // },
-  {
-    "code": ".5.7",
-    "name": "sus4"
-  },
-  {
-    "code": ".2.7",
-    "name": "sus2"
-  },
-  {
-    "code": ".1.7",
-    "name": "no3/5(♭9)"
-  },
-  {
-    "code": ".6.7",
-    "name": "no3/5(#11)"
-  },
-  {
-    "code": ".6.7.11",
-    "name": "no3/5/maj7(#11)"
-  },
-  {
-    "code": ".7.10",
-    "name": "no3/5 7"
-  },
-  {
-    "code": ".4.7.14",
-    "name": "(add9)"
-  },
-  {
-    "code": ".4.7.9",
-    "name": "6"
-  },
-  {
-    "code": ".4.7.11",
-    "name": "maj7"
-  },
-  {
-    "code": ".4.7.11.2",
-    "name": "maj7/9"
-  },
-  {
-    "code": ".5.7.11",
-    "name": "sus4/maj7"
-  },
-  {
-    "code": ".7.11",
-    "name": "no3/5/maj7"
-  },
-  {
-    "code": ".3.7",
-    "name": "m"
-  },
-  {
-    "code": ".3.7.13",
-    "name": "m add ♭9"
-  },
-  {
-    "code": ".3.7.14",
-    "name": "m add9"
-  },
-  {
-    "code": ".3.7.17",
-    "name": "m add11"
-  },
-  {
-    "code": ".3.7.9",
-    "name": "m6"
-  },
-  {
-    "code": ".3.7.8",
-    "name": "m ♭6"
-  },
-  {
-    "code": ".3.7.10",
-    "name": "m7"
-  },
-  {
-    "code": ".3.7.11",
-    "name": "m maj7"
-  },
-  {
-    "code": ".3.6.10",
-    "name": "m7 ♭5"
-  },
-  {
-    "code": ".4.7.10",
-    "name": "7"
-  },
-  {
-    "code": ".2.7.9",
-    "name": "6 sus2"
-  },
-  {
-    "code": ".5.7.10",
-    "name": "7 sus4"
-  },
-  {
-    "code": ".4.8",
-    "name": "aug"
-  },
-  {
-    "code": ".3.6",
-    "name": "dim"
-  },
-  {
-    "code": ".3.6.9",
-    "name": "dim7"
-  },
-  {
-    "code": ".3.6.13",
-    "name": "dim ♭9"
-  },
-  {
-    "code": ".3.6.14",
-    "name": "dim 9"
-  },
-  {
-    "code": ".3.6.17",
-    "name": "dim 11"
-  },
-  // {
-  //   "code": ".3.8",
-  //   "name": "major first inversion"
+  //   name: 'thirteenth-flat-ninth',
+  //   code: '0.4.7.10.13.NaN.NaN',
+  //   quality: 'major'
   // },
   // {
-  //   "code": ".5.9",
-  //   "name": "major second inversion"
-  // }
+  //   name: 'thirteenth-flat-ninth-flat-fifth',
+  //   code: '0.4.6.10.13.NaN.NaN',
+  //   quality: 'm3+d5'
+  // },
+  { name: 'tonic-counter-parallel', code: '0.3.7', quality: 'minor' },
+  { name: 'tonic', code: '0.4.7', quality: 'major' },
+  { name: 'tonic-parallel', code: '0.3.7', quality: 'minor' },
+  { name: 'tristan', code: '0.3.6.10', quality: 'predominant' },
+  { name: 'viennese-tri', code: '0.6.7', quality: 'atonal' }
 ]
 
 const sortingArr = [ "C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5","C6","C#6","D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6" ]
 
-
 const makeChordCode = (...arr) => {
-  // takes array of musical notes, return semitone intervals
+  // takes array of musical notes,
+  // e.g. ["C4", "D#4", "G4"]
+  // returns semitone intervals (root note included in output)
+  //e.g. [4, 7]
   let chord = []
   arr.map(c => chord.push(sortingArr.indexOf(c)))
-  return chord.map(c=>c-chord[0]).filter(c=>c!==0)
+  return chord.map(c=>c-chord[0])
 }
 
 const findChordTypes = str => {
   // str is the chord code e.g. ".4.7.11"
-  // theResult is an object e.g. {code: ".5.7", name: "sus4"}
-  const theResult = chordTypes.find((el) => el.code === str)
-  if (theResult !== undefined)
-    return theResult.name
+  // chord is an object e.g. {code: ".5.7", name: "sus4"}
+  const chord = chordTypes.find((el) => el.code === str)
+  return chord === undefined ? `` :chord.name
 }
 
 //
@@ -219,7 +207,7 @@ const addNote = e => {
       .sort((a,b)=>sortingArr.indexOf(a)-sortingArr.indexOf(b)))
       .join(".")
       .toString()
-    theChordCode = `.`.concat(theChordCode)
+      console.log(theChordCode)
     if (theChordCode !== ``) {
       result.innerHTML = `Chord: ${notesArr[0].replace(/\d/g, '')} ${findChordTypes(theChordCode)}`
     }
@@ -229,18 +217,16 @@ const addNote = e => {
 const removeNote = e => {
   if (document.querySelector(`.key[data-key="${e.keyCode}"]`)) {
     let oneNote = document.querySelector(`.key[data-key="${e.keyCode}"] .js-note`).textContent
-
     allowed = true
-
     if (oneNote && skippedNote !== true) {
       notesArr.pop(oneNote)
     }
-
     skippedNote = false
-
-    let theChordCode = (makeChordCode(...notesArr)).join(`.`).toString()
-    theChordCode = `.`.concat(theChordCode)
-
+    let theChordCode = makeChordCode(...notesArr
+      .sort((a,b)=>sortingArr.indexOf(a)-sortingArr.indexOf(b)))
+      .join(".")
+      .toString()
+    console.log(theChordCode)
     if (notesArr === undefined || notesArr.length === 0) {
       result.innerHTML = `Chord:`
     } else {
@@ -268,3 +254,4 @@ window.addEventListener("keyup", e => {
   removeNote(e)
   removePlaying(e)
 })
+
